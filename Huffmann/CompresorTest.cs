689d91cc5957106e2af15ed780d3f087fa63f78d
mishell <mmq.llumitaxi@yavirac.edu.ec>
2020-12-18 17:48:46 -0500

    Agregar archivos de proyecto.

---
+++ Huffmann/CompresorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace Huffmann
+{
+    [TestClass]
+    public class CompresorTest
+    {
+        [TestMethod]
+        public void CuandoAplicoHuffmanTamañoFinalMenorQueTamañoInicial()
+        {
+            var cadenaSinComprimir = "EXTENUANTE";
+            var tamañoInicial = cadenaSinComprimir.Length * sizeof(char);
+
+            var compresor = new Compresor();
+            BitArray cadenaComprimida = compresor.Huffman(cadenaSinComprimir);
+
+            Assert.IsNotNull(cadenaComprimida);
+            Assert.IsTrue(cadenaComprimida.Length > 0);
+            Assert.IsTrue(cadenaComprimida.Length < tamañoInicial);
+        }
+    }
+}
